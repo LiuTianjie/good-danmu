47ca430df8d75ca56e6702acbd5635f8d6b449f1
NIckname4th <2497957861@qq.com>
2021-05-30 12:22:26 +0800

    feature：新功能

--- main.go
+++ main.go
@@ -4,13 +4,23 @@
  * @Author: Nickname4th
  * @Date: 2021-05-22 16:47:46
  * @LastEditors: Nickname4th
- * @LastEditTime: 2021-05-23 20:01:41
+ * @LastEditTime: 2021-05-28 16:55:45
  */
 package main
 
-import in "good-danmu/src/init"
+import (
+	"good-danmu/src/global"
+	in "good-danmu/src/init"
+)
 
 func main() {
+	global.VP = in.Viper()
+	global.DB = in.ConnectDB()
+	if global.DB != nil {
+		in.Gorm(global.DB)
+		db, _ := global.DB.DB()
+		defer db.Close()
+	}
 	router := in.Routers()
 	router.Run(":8000")
 }
