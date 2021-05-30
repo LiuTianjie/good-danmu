083d43def01aa7667d88347d88a5067c494bf42e
NIckname4th <2497957861@qq.com>
2021-05-30 12:28:47 +0800

    nothing

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module good-danmu
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fsnotify/fsnotify v1.4.7
 	github.com/gin-gonic/gin v1.7.2
 	github.com/gorilla/websocket v1.4.2
 	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/viper v1.7.1
 	golang.org/x/text v0.3.2
+	gorm.io/driver/mysql v1.1.0
+	gorm.io/gorm v1.21.10
 )
--- src/init/db.go
+++ src/init/db.go
@@ -4,8 +4,48 @@
  * @Author: Nickname4th
  * @Date: 2021-05-22 16:54:30
  * @LastEditors: Nickname4th
- * @LastEditTime: 2021-05-23 21:21:49
+ * @LastEditTime: 2021-05-28 16:59:55
  */
 package init
 
-// TODO, add some thing about dbs.
+import (
+	"good-danmu/src/global"
+	"good-danmu/src/model"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	m := global.CONFIG.Mysql
+	if m.Dbname == "" {
+		return nil
+	}
+	dsn := m.Username + ":" + m.Password + "@tcp(" + m.Path + ")/" + m.Dbname + "?" + m.Config
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       dsn,   // DSN data source name
+		DefaultStringSize:         256,   // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
+	}), &gorm.Config{})
+	if err != nil {
+		log.Println("MySql启动错误")
+		return nil
+	}
+	return db
+}
+
+func Gorm(db *gorm.DB) {
+	err := db.AutoMigrate(
+		model.User{},
+	)
+	if err != nil {
+		log.Println("初始化数据库失败")
+		os.Exit(0)
+	}
+	log.Println("初始化数据库成功")
+}
