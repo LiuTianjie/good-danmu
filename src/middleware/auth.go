5843a64f6675c6c9dd1fa234fce86677041edc25
NIckname4th <2497957861@qq.com>
2021-05-30 12:17:23 +0800

    feature：jwt结构和鉴权中间件

--- src/middleware/auth.go
+++ src/middleware/auth.go
@@ -4,8 +4,109 @@
  * @Author: Nickname4th
  * @Date: 2021-05-22 16:54:00
  * @LastEditors: Nickname4th
- * @LastEditTime: 2021-05-23 21:22:37
+ * @LastEditTime: 2021-05-28 17:53:06
  */
 package middleware
 
-// TODO: build auth system.
+import (
+	"errors"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"good-danmu/src/global"
+	"good-danmu/src/model"
+	"good-danmu/src/utils"
+	"log"
+	"strconv"
+	"time"
+)
+
+type JWT struct {
+	SignKey []byte
+}
+
+var (
+	TokenExpired     = errors.New("Token已过期")
+	TokenNotValidYet = errors.New("Token未生效")
+	TokenMalformed   = errors.New("Token 非法")
+	TokenInvalid     = errors.New("无法解析Token")
+)
+
+func JWTAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("token")
+		if token == ""{
+			token = c.Request.Header.Get("Sec-WebSocket-Protocol")
+		}
+		log.Println("token is ",token)
+		if token == "" {
+			utils.FailedMsg(401, "未授权的访问", c)
+			c.Abort()
+			return
+		}
+		j := NewJWT()
+		claims, err := j.ParseJWT(token)
+		if err != nil {
+			if err == TokenExpired {
+				utils.FailedMsg(401, "授权已过期", c)
+				c.Abort()
+				return
+			}
+			utils.FailedMsg(401, "认证失败", c)
+			c.Abort()
+			return
+		}
+		var u model.User
+		if err = global.DB.Where("`uuid` = ?", claims.UUID.String()).First(&u).Error; err != nil {
+			utils.FailedMsg(401, "认证失败", c)
+			c.Abort()
+		}
+		if claims.ExpiresAt-time.Now().Unix() < claims.BufferTime {
+			claims.ExpiresAt = time.Now().Unix() + global.CONFIG.JWT.ExpiresTime
+			newToken, _ := j.CreateJWT(*claims)
+			newClaims, _ := j.ParseJWT(newToken)
+			c.Header("new-token", newToken)
+			c.Header("new-expires-at", strconv.FormatInt(newClaims.ExpiresAt, 10))
+		}
+		c.Set("claims", claims)
+		c.Next()
+	}
+}
+
+func NewJWT() *JWT {
+	return &JWT{
+		[]byte(global.CONFIG.JWT.SignKey),
+	}
+}
+
+func (j *JWT) CreateJWT(claims model.CustomClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(j.SignKey)
+}
+
+func (j *JWT) ParseJWT(tokenString string) (*model.CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &model.CustomClaims{}, func(token *jwt.Token) (i interface{}, e error) {
+		return j.SignKey, nil
+	})
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, TokenMalformed
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				return nil, TokenExpired
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, TokenNotValidYet
+			} else {
+				return nil, TokenInvalid
+			}
+		}
+	}
+	if token != nil {
+		if claims, ok := token.Claims.(*model.CustomClaims); ok && token.Valid {
+			return claims, nil
+		}
+		return nil, TokenInvalid
+
+	} else {
+		return nil, TokenInvalid
+	}
+}
---
+++ src/model/jwt.go
@@ -0,0 +1,23 @@
+/*
+ * @Descripttion: your project
+ * @version: 1.0
+ * @Author: Nickname4th
+ * @Date: 2021-05-28 17:44:21
+ * @LastEditors: Nickname4th
+ * @LastEditTime: 2021-05-29 10:24:56
+ */
+package model
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	uuid "github.com/satori/go.uuid"
+)
+
+// Custom claims structure
+type CustomClaims struct {
+	UUID       uuid.UUID
+	ID         uint
+	Username   string
+	BufferTime int64
+	jwt.StandardClaims
+}
