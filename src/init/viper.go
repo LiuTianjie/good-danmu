0b2f3bab3d1707c8a144cc028f33f18001677b2c
NIckname4th <2497957861@qq.com>
2021-05-30 12:17:44 +0800

    feature:配置读取

--- src/init/viper.go
+++ src/init/viper.go
@@ -4,8 +4,33 @@
  * @Author: Nickname4th
  * @Date: 2021-05-22 16:55:05
  * @LastEditors: Nickname4th
- * @LastEditTime: 2021-05-23 21:22:24
+ * @LastEditTime: 2021-05-28 17:31:17
  */
 package init
 
-// TODO: Use viper to read the config from config files.
+import (
+	"fmt"
+	"good-danmu/src/global"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+func Viper(path ...string) *viper.Viper {
+	config := "config.yaml"
+	v := viper.New()
+	v.SetConfigFile(config)
+	if err := v.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("配置文件读取错误"))
+	}
+	v.WatchConfig()
+	v.OnConfigChange(func(e fsnotify.Event) {
+		if err := v.Unmarshal(&global.CONFIG.JWT); err != nil {
+			fmt.Println(err)
+		}
+	})
+	if err := v.Unmarshal(&global.CONFIG); err != nil {
+		fmt.Println(err)
+	}
+	return v
+}
