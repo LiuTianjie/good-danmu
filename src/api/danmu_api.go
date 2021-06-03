53b272f6e984922742697ecf0bf971e11b22b10c
NIckname4th <2497957861@qq.com>
2021-06-03 18:47:00 +0800

    fix&feature:增加用户查询功能，精简数据结构，抽离gorm操作

--- src/api/danmu_api.go
+++ src/api/danmu_api.go
@@ -10,22 +10,20 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"good-danmu/src/global"
+	h "good-danmu/src/handler"
 	"good-danmu/src/model"
 	"good-danmu/src/utils"
 	"good-danmu/src/utils/parse"
 	"log"
 )
 
+type RoomStruct struct {
+	RoomName string
+	RoomDesc string
+}
+
 func GetDanmuRooms(c *gin.Context) {
-	type ans struct {
-		DanmuId     string `json:"danmu_id"`
-		RoomName    string `json:"room_name"`
-		RoomDesc    string `json:"room_desc"`
-		OnlineUsers int    `json:"online_users"`
-		DanmuCount  int    `json:"danmu_count"`
-	}
-	var rooms []ans
-	global.DB.Table("danmu_rooms").Select([]string{"danmu_id", "room_name", "room_desc", "online_users, danmu_count"}).Scan(&rooms)
+	rooms := h.SearchAllRooms()
 	//utils.OkMsg(200, "请求成功", c)
 	ret, _ := json.Marshal(rooms)
 	n := len(ret)        //Find the length of the byte array
@@ -36,13 +34,34 @@ func GetDanmuRooms(c *gin.Context) {
 func CreateDanmuRoom(c *gin.Context) {
 	//	TODO: use casbin to create more complex auth
 	//	 here use my phone to simply control danmu room's creation.
-	token := c.GetHeader("token")
-	room := c.ShouldBind(&model.DanmuRoom{})
-	Username, err := parse.GetUserFromToken(token)
+	var (
+		err      error
+		Username string
+		room     RoomStruct
+		token    string
+	)
+	if err = c.ShouldBind(&room); err != nil {
+		utils.FailedMsg(400, "参数错误", c)
+	}
+	token = c.GetHeader("token")
+	Username, err = parse.GetUserFromToken(token)
 	if err != nil || Username != "17330929598" {
 		utils.FailedMsg(401, "权限不足", c)
 		c.Abort()
 		return
+	} else {
+		var roomInfo = model.DanmuRoom{
+			RoomName: room.RoomName,
+			RoomDesc: room.RoomDesc,
+		}
+		if err = global.DB.Create(&roomInfo).Error; err != nil {
+			utils.FailedMsg(400, "创建错误", c)
+			c.Abort()
+			return
+		} else {
+			rooms := h.SearchAllRooms()
+			utils.OkDetail(200, rooms, "创建成功！", c)
+		}
 	}
-	log.Println(room)
+	log.Println("room", room)
 }
