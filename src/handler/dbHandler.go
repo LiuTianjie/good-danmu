94023ddb435b215b31cad4b8768da8d4e900acb2
NIckname4th <2497957861@qq.com>
2021-06-03 18:49:17 +0800

    fix&feature:单独一个文件存放gorm操作

---
+++ src/handler/dbHandler.go
@@ -0,0 +1,54 @@
+/*
+Package handler
+@Time : 2021/6/3 16:50
+@Author : nickname4th
+@File : dbHandler
+@Software: GoLand
+*/
+package handler
+
+import (
+	"good-danmu/src/global"
+	"good-danmu/src/model"
+	"good-danmu/src/utils"
+)
+
+// 返回房间信息结构体
+type room struct {
+	ID          string `json:"id"`
+	RoomName    string `json:"room_name"`
+	RoomDesc    string `json:"room_desc"`
+	OnlineUsers int    `json:"online_users"`
+	DanmuCount  int    `json:"danmu_count"`
+}
+
+func SearchAllRooms() []room {
+	var rooms []room
+	global.DB.Table("danmu_rooms").Select([]string{"id", "room_name", "room_desc", "online_users, danmu_count"}).Scan(&rooms)
+	return rooms
+}
+
+func SearchUser(mode string, arg interface{}) (data model.User, err error) {
+	switch mode {
+	case "ByPass":
+		{
+			L := arg.(model.User)
+			err = global.DB.Where("username = ? AND password = ?", L.Username, utils.MD5V([]byte(L.Password))).First(&data).Error
+		}
+	case "ByName":
+		{
+			L := arg.(string)
+			err = global.DB.Where("username = ?", L).First(&data).Error
+		}
+	case "All":
+		{
+			err = global.DB.Table("users").Select("*").Scan(&data).Error
+		}
+	case "Existed":
+		{
+			L := arg.(string)
+			err = global.DB.Where("username = ?", L).First(&data).Error
+		}
+	}
+	return data, err
+}
